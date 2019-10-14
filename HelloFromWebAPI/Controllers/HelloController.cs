539b0934521a9460e55bd1ff9d50dd13fd0a5253
jzes <pauloneto.jose@gmail.com>
2019-10-13 21:14:51 -0300

    exemplo de api em csharp, vai fazer sucesso com o pessoal

---
+++ HelloFromWebAPI/Controllers/HelloController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloFromWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        // GET api/hello
+        [HttpGet]
+        public ActionResult<Object> Get()
+        {
+            return Ok(new {
+                sucesso = true,
+                nome = "",
+                mensagem = $"Hello"
+            });
+        }
+
+        // GET api/hello/pirelinho
+        [HttpGet("{name}")]
+        public ActionResult<Object> Get(string name)
+        {
+            return Ok(new {
+                sucesso = true,
+                nome = name,
+                mensagem = $"Hello {name}"
+            });
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
